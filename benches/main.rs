4233cd6883cd3479006ae24308d69cb818ba7004
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-09-24 18:58:20 +0900

    Add fxhash

--- benches/main.rs
+++ benches/main.rs
@@ -1,7 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use fxhash::FxHashMap;
 use hashbrown::HashMap as BrownHashMap;
 use rand::prelude::*;
-use rustc_hash::FxHashMap;
+use rustc_hash::FxHashMap as RustcHashMap;
 
 fn hash_map_insert_brown(input: &Vec<u64>) {
     let mut map_brown: BrownHashMap<u64, u64> = BrownHashMap::default();
@@ -16,6 +17,18 @@ fn hash_map_get_brown(input: &Vec<u64>, map: &BrownHashMap<u64, u64>) {
     }
 }
 
+fn hash_map_insert_rustc(input: &Vec<u64>) {
+    let mut map_fx = RustcHashMap::default();
+    for &item in input {
+        map_fx.insert(item, item);
+    }
+}
+
+fn hash_map_get_rustc(input: &Vec<u64>, map: &RustcHashMap<u64, u64>) {
+    for &item in input {
+        black_box(map.get(&item));
+    }
+}
 fn hash_map_insert_fx(input: &Vec<u64>) {
     let mut map_fx = FxHashMap::default();
     for &item in input {
@@ -31,13 +44,18 @@ fn hash_map_get_fx(input: &Vec<u64>, map: &FxHashMap<u64, u64>) {
 
 fn benchmark(c: &mut Criterion) {
     let mut rng = thread_rng();
-    let input: Vec<u64> = (0..100_000).map(|_| rng.gen_range(0..100_000)).collect();
+    let input: Vec<u64> = (0..1_000_000).map(|_| rng.gen_range(0..100_000)).collect();
 
     let map_brown = input
         .iter()
         .copied()
         .zip(input.iter().copied())
         .collect::<BrownHashMap<_, _>>();
+    let map_rustc = input
+        .iter()
+        .copied()
+        .zip(input.iter().copied())
+        .collect::<RustcHashMap<_, _>>();
     let map_fx = input
         .iter()
         .copied()
@@ -51,9 +69,13 @@ fn benchmark(c: &mut Criterion) {
         b.iter(|| hash_map_get_brown(&input, &map_brown))
     });
     c.bench_function("rustc_hash insert", |b| {
-        b.iter(|| hash_map_insert_fx(&input))
+        b.iter(|| hash_map_insert_rustc(&input))
     });
     c.bench_function("rustc_hash get", |b| {
+        b.iter(|| hash_map_get_rustc(&input, &map_rustc))
+    });
+    c.bench_function("fxhash insert", |b| b.iter(|| hash_map_insert_fx(&input)));
+    c.bench_function("fxhash get", |b| {
         b.iter(|| hash_map_get_fx(&input, &map_fx))
     });
 }
